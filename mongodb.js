9b702f157180ed72815628bc4f096ce6e9b89831
unknown <sazwerling@gmail.com>
2022-08-20 11:26:14 -0700

    use insertMany method on db.collection

--- mongodb.js
+++ mongodb.js
@@ -1,19 +1,63 @@
 // CRUD -- CREATE READ UPDATE DELETE
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
-const connectionURL = 'mongodb://127.0.0.1:27017'
-const databaseName = 'task-manager'
+const connectionURL = "mongodb://127.0.0.1:27017";
+const databaseName = "task-manager";
 
-MongoClient.connect(connectionURL, { useUnifiedTopology: true }, { useNewUrlParser: true}, (error, client) => {
-    if(error){
-        return console.log('Unable to conect to database')
-    } else {
-        const db = client.db(databaseName)
+MongoClient.connect(
+   connectionURL,
+   { useUnifiedTopology: true },
+   { useNewUrlParser: true },
+   (error, client) => {
+      if (error) {
+         return console.log("Unable to conect to database");
+      }
 
-        db.collection('users').insertOne(({
-            name: 'Seth',
-            age: 46
-        }))
-    }
-})
+      const db = client.db(databaseName);
+
+      console.log("connected");
+
+        db.collection("tasks").insertMany([
+            {
+                task: "clean garage",
+                completed: false
+            },
+            {
+                task: "eat breakfast",
+                completed: true
+            },
+            {
+                task: "apply for jobs",
+                completed: true
+            }
+        ],(error, result) => {
+            if(error){
+                return console.log('there was an error')
+            }
+            console.log(result.ops)
+        })
+
+    //   db.collection("users").insertOne(
+    //      {
+    //         name: "Johnny",
+    //         age: 132,
+    //      },
+    //      (error, result) => {
+    //         if (error) {
+    //            return `error: ${error}`;
+    //         }
+    //         console.log(result.ops);
+    //         console.log(result.insertedCount)
+    //      }
+    //   );
+    
+   }
+);
+
+
+
+// INSERT THREE TASKS INTO A NEW TASKS COLLECTION
+// description (string), completed (boolean)
+// callback to handle error or print ops property
+
