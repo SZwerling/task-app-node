fb1316bae40d1002059937f87c376ab0cc8860a7
unknown <sazwerling@gmail.com>
2022-08-24 13:54:09 -0700

    create mongoose model for tasks collection

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -13,13 +13,33 @@ const User = mongoose.model('User', {
     }
 })
 
+const Task = mongoose.model('Task', {
+    task: {
+        type: String
+    },
+    completed: {
+        type: Boolean
+    }
+})
+
+const newTask = new Task({
+    task: 'Leg exercises',
+    completed: false
+})
+
 const me = new User({
     name: 'Juanito',
     age: 33
 })
 
-me.save().then(() => {  // we could put the me in the .then callback, but we already have access to it.
-    console.log(me)
+newTask.save().then(() => {
+    console.log(newTask)
 }).catch((error) => {
-    console.log('error', error)
+    console.log(error)
 })
+
+// me.save().then(() => {  // we could put the me in the .then callback, but we already have access to it.
+//     console.log(me)
+// }).catch((error) => {
+//     console.log('error', error)
+// })
