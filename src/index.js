aee5f4d628b1e462ba18eba95bee098e7e7dd9c4
unknown <sazwerling@gmail.com>
2022-08-25 11:26:30 -0700

    change creation resource success code to 201/success resource created

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ app.post('/users', (req, res) => {
     const user =  new User(req.body);
 
     user.save().then(() => {
-        res.send(user)
+        res.status(201).send(user)
     }).catch((e) => {
         res.status(400).send(e) // must set status before send()
     })
@@ -23,7 +23,7 @@ app.post('/tasks', (req, res) => {
     const task = new Task(req.body);
 
     task.save().then(() => {
-        res.send(task)
+        res.status(201).send(task)
     }).catch((e) => {
         res.status(400).send(e)
     })
