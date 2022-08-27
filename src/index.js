b089c5b41f81734d67b3fe41ea6b0b647233130d
unknown <sazwerling@gmail.com>
2022-08-27 08:36:38 -0700

    create reading endpoints for tasks and tasks/id

--- src/index.js
+++ src/index.js
@@ -39,11 +39,30 @@ app.get('/users/:id', (req, res) => {
 
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body);
-
     task.save().then(() => {
         res.status(201).send(task)
     }).catch((e) => {
         res.status(400).send(e)
+    })
+})
+
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id
+    Task.findById(_id).then((task) => {
+        if(!task){
+            return res.status(404).send()
+        }
+        res.send(task)
+    }).catch((e) => {
+        res.status(500).send()
     })
 })
 
