a2e17fec43f4cfd8810a3e274c9c32da9180f9c5
unknown <sazwerling@gmail.com>
2022-08-31 09:46:52 -0700

    add patch endpoint to users route

--- src/index.js
+++ src/index.js
@@ -1,71 +1,111 @@
-const express = require('express')
-require('./db/mongoose')
-const User = require('./models/user')
-const Task = require('./models/task')
+const express = require("express");
+require("./db/mongoose");
+const User = require("./models/user");
+const Task = require("./models/task");
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(express.json())
+app.use(express.json());
 
-app.post('/users', (req, res) => {
-    const user =  new User(req.body);
-    user.save().then(() => {
-        res.status(201).send(user)
-    }).catch((e) => {
-        res.status(400).send(e) // must set status before send()
-    })
+app.post("/users", async (req, res) => {
+   const user = new User(req.body)
+   
+   try {
+      await user.save();
+      res.status(201).send(user);
+   } catch (e) {
+      res.status(400).send(e);
+   }
+});
+
+app.get("/users", async (req, res) => {
+   try {
+      const users = await User.find({});
+      res.send(users);
+   } catch (e) {
+      res.status(500).send();
+   }
+});
+
+app.get("/users/:id", async (req, res) => {
+   const _id = req.params.id;
+   try {
+    const user = await User.findById(_id)
+    if (!user) {
+        return res.status(404).send();
+     }
+     res.send(user);
+   } catch (e) {
+    res.status(500).send();
+   }
+});
+
+app.patch("/users/:id", async (req, res) => {
+   //CHECK IF DESIRED UPDATE IS ALLOWED //
+   const updates = Object.keys(req.body)  //converts keys of req object to array of those keys
+   const allowedUpdates = ['name', 'email', 'password', 'age'] //arr of allowed updates so we can compare
+   const isValid = updates.every((item) => allowedUpdates.includes(item)) //returns boolean as to if every desired updated exists in allowed arr 
+   if(!isValid){
+      return res.status(400).send({ error: 'Invalid Updates!' })
+   }
+   /////////////////
+
+   try {
+      const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true }) 
+      
+      if(!user){                                //find by id   //update info  //send back updated user //run validators on update info
+         return res.status(404).send()
+      }
+
+      res.send(user)
+   } catch (e) {
+      res.status(400).send(e)
+   }
 })
 
-app.get('/users', (req, res) => {
-    User.find({}).then((users) => {
-        res.send(users)
-    }).catch((e) => {
-        res.status(500).send()
-    })
-})
+app.post("/tasks", async (req, res) => {
+   const task = new Task(req.body);
+   try {
+    await task.save()
+    res.status(201).send(task);
+   } catch (e) {
+    res.status(400).send(e);
+   }
+});
 
-app.get('/users/:id', (req, res) => {
-    const _id = req.params.id
-    User.findById(_id).then((user) => {
-        if(!user){
-            return res.status(404).send();
-        }
-        res.send(user)
-    }).catch((e) => {
-        res.status(500).send()
-    })
-})
+app.get("/tasks", async (req, res) => {
+    try {
+        const tasks = await Task.find({})
+        res.send(tasks);
+    } catch (e) {
+        res.status(500).send();
+    }
+});
 
-app.post('/tasks', (req, res) => {
-    const task = new Task(req.body);
-    task.save().then(() => {
-        res.status(201).send(task)
-    }).catch((e) => {
-        res.status(400).send(e)
-    })
-})
-
-app.get('/tasks', (req, res) => {
-    Task.find({}).then((tasks) => {
-        res.send(tasks)
-    }).catch((e) => {
-        res.status(500).send()
-    })
-})
-
-app.get('/tasks/:id', (req, res) => {
-    const _id = req.params.id
-    Task.findById(_id).then((task) => {
-        if(!task){
-            return res.status(404).send()
-        }
-        res.send(task)
-    }).catch((e) => {
-        res.status(500).send()
-    })
-})
+app.get("/tasks/:id", async (req, res) => {
+   const _id = req.params.id;
+   try {
+    const task = await Task.findById(_id)
+    if (!task) {
+        return res.status(404).send();
+     }
+     res.send(task);
+   } catch (e) {
+    res.status(500).send();
+   }
+});
 
 app.listen(port, () => {
-    console.log(`App is up on port ${port}`)
-})
+   console.log(`App is up on port ${port}`);
+});
+
+// THIS IS THE CODE BEFORE ASYNC REFACTOR // (req, res) IS THE START OF THE FUNCTION THAT GETS PASSED TO EXPRESS VIA APP.POST
+// app.post('/users', (req, res) => {
+//     const user =  new User(req.body);
+//     user.save().then(() => {
+//         res.status(201).send(user)
+//     }).catch((e) => {
+//         res.status(400).send(e) // must set status before send()
+//     })
+// })
