f1364ebb7df07a77c8b1bd696af243ed0942a66d
unknown <sazwerling@gmail.com>
2022-08-30 23:15:42 -0700

    refactor all endpoints to use async/await

--- playground/promise-chaining2.js
+++ playground/promise-chaining2.js
@@ -4,11 +4,23 @@ const Task = require('../src/models/task')
 
 
 
-Task.findByIdAndDelete('63069efcb199aa6915406e17').then((deleted) => {
-    console.log(deleted)
-    return  Task.countDocuments({ completed: false})
-}).then((result) => {
-   console.log(result)
+// Task.findByIdAndDelete('63069efcb199aa6915406e17').then((deleted) => {
+//     console.log(deleted)
+//     return  Task.countDocuments({ completed: false})
+// }).then((result) => {
+//    console.log(result)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+const deleteTaskAndCout = async (id) => {
+    await Task.findByIdAndDelete(id)    // do not need to store returned value from await in a variable eg const del = await Task.findByYourMommaAndDelete
+    const count = await Task.countDocuments({ completed: false })
+    return count
+}
+
+deleteTaskAndCout('630a341379cbd0757c5335e6').then((count) => {
+    console.log(count)
 }).catch((e) => {
     console.log(e)
 })
