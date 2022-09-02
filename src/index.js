95813156d97c07251659621c97d5f24caa238106
unknown <sazwerling@gmail.com>
2022-09-02 16:50:42 -0700

    add patch endpoint for tasks

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+
+///USERS ENDPOINTS
 app.post("/users", async (req, res) => {
    const user = new User(req.body)
    
@@ -50,10 +52,8 @@ app.patch("/users/:id", async (req, res) => {
       return res.status(400).send({ error: 'Invalid Updates!' })
    }
    /////////////////
-
    try {
       const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true }) 
-      
       if(!user){                                //find by id   //update info  //send back updated user //run validators on update info
          return res.status(404).send()
       }
@@ -64,6 +64,8 @@ app.patch("/users/:id", async (req, res) => {
    }
 })
 
+
+///TASKS ENDPOINT
 app.post("/tasks", async (req, res) => {
    const task = new Task(req.body);
    try {
@@ -96,9 +98,33 @@ app.get("/tasks/:id", async (req, res) => {
    }
 });
 
+app.patch("/tasks/:id", async (req, res) => {
+   const _id = req.params.id;
+   const allowedUpdates = ['task', 'completed']
+   const updates = Object.keys(req.body)
+   const isValid = updates.every((item) => allowedUpdates.includes(item))
+   if(!isValid){
+      return res.status(400).send({ error: 'Invalid Updates!' })
+   }
+
+   try {
+      const task = await Task.findByIdAndUpdate(_id, req.body, { new: true, runValidators: true }) 
+      if(!task){                                //find by id   //update info  //send back updated user //run validators on update info
+         return res.status(404).send()
+      }
+      res.send(task)
+   } catch (e) {
+      res.status(400).send(e)
+   }
+})
+
+
 app.listen(port, () => {
    console.log(`App is up on port ${port}`);
 });
+
+
+
 
 // THIS IS THE CODE BEFORE ASYNC REFACTOR // (req, res) IS THE START OF THE FUNCTION THAT GETS PASSED TO EXPRESS VIA APP.POST
 // app.post('/users', (req, res) => {
