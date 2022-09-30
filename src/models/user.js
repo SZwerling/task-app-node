01854b4426f73c34657d7b6aa266848d3c214bd4
unknown <sazwerling@gmail.com>
2022-09-30 10:47:38 -0700

    remove all tasks when associated user deletes profile

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken')
+const Task = require('./task')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -97,6 +98,13 @@ userSchema.pre('save', async function(next){
     if(this.isModified('password')){
         this.password = await bcrypt.hash(this.password, 8)
     }
+    next()
+})
+
+// DELETE TASKS WHEN USER IS REMOVED
+userSchema.pre('remove', async function(next){
+    const user = this;
+    await Task.deleteMany({ owner: user._id })
     next()
 })
 
