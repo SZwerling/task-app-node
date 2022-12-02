317ddf90ff45d93b1fb09b3eaba4ef4155454c5f
unknown <sazwerling@gmail.com>
2022-12-02 15:33:38 -0800

    attempt task schema

--- src/models/task.js
+++ src/models/task.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 
 
-const Task = mongoose.model('Task', {
+const taskSchema = mongoose.Schema({
     task: {
         type: String,
         trim: true,
@@ -17,7 +17,11 @@ const Task = mongoose.model('Task', {
         required: true,
         ref: 'User'
     }
+}, {
+    timestamps: true
 })
+
+const Task = mongoose.model('Task', taskSchema)
 
 
 module.exports = Task
